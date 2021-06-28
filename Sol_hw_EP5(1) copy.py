28261a3bdf6be227acc1f23d5d90b93010afdb9f
IsoonSer <86610808+IsoonSer@users.noreply.github.com>
2021-06-28 21:00:50 +0700

    อัพโหลดโปรแกรมครั้งแรก
    สามารถบันทึกและแสดงรายจ่ายได้แล้ว

---
+++ Sol_hw_EP5(1) copy.py
@@ -0,0 +1,258 @@
+from tkinter import *
+# from tkinter.ttk import Notebook
+from tkinter import ttk, messagebox
+import csv
+from datetime import datetime
+
+root = Tk()
+root.title("Expense By @IS")
+root.geometry("600x800+3100+0") # 450
+
+################ Menu Bar ################
+menu_bar = Menu(root)
+root.config(menu=menu_bar) # familiar .pack()
+
+# Menu file
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File",menu=file_menu)
+file_menu.add_command(label="Import CSV")
+
+# Help
+def About():
+    messagebox.showinfo("About","สวัสดีครับ โปรแกรมนี้คือโปรแกรมบันทึกข้อมูลรายจ่าย\nสนในบริจาคให้เราไหม ขอ 1 บิทคอยก็พอ")
+
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help",menu=help_menu)
+help_menu.add_command(label="About", command=About)
+
+# Donate
+donate_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Donate",menu=donate_menu)
+
+
+
+##########################################
+
+
+def Save(event=None):
+    dt_d = datetime.now().strftime("%m-%d-%Y")
+    dt_t = datetime.now().strftime("%H:%M:%S")
+    # dt = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+    expense = v_expense.get()
+    price = v_price.get()
+    quantity = v_quantity.get()
+    # price = int(price)
+    # quantity = int(quantity)
+    
+    if quantity == '':
+        quantity = 1
+    elif expense == '' or price == '':
+        messagebox.showwarning("Error", "กรุณากรอกข้อมูลให้ครบ (รายการและราคาต่อชิ้น)")
+        return
+    try :
+        total_price = int(price) * int(quantity)
+    except :
+        messagebox.showerror("Error", "กรุณากรอกตัวเลขใหม่ คุณกรอกตัวเลขผิด")
+        v_expense.set('')
+        v_price.set('')
+        v_quantity.set('')
+        # messagebox.showwarning("Error", "กรุณากรอกตัวเลขใหม่ คุณกรอกตัวเลขผิด")
+        # messagebox.showinfo("Error", "กรุณากรอกตัวเลขใหม่ คุณกรอกตัวเลขผิด")
+
+    text = f"วันที่ {dt_d} เวลา: {dt_t} \nรายการ: {expense} ราคาต่อชิ้น: {price} บาท \nจำนวน: {quantity} ชิ้น ราคารวม: {total_price} บาท\n" 
+    v_result.set(text)
+    
+    # text_p2 = v_show.get() # ดึงเอา v_show มาเพิ่มตัวที่อัพใหม่
+    # text_p2 += text
+    # v_show.set(text_p2)
+    
+    
+    # print(f"วันที่ {dt_d} เวลา: {dt_t}  รายการ: {expense} ราคา/ชิ้น: {price} บาท ราคารวม: {total_price} บาท ")
+
+    # Save Data to csv
+    with open("data.csv", 'a', encoding='utf-8', newline='') as f:
+        fw = csv.writer(f)
+        data = [dt_d, dt_t, expense, price, quantity, total_price]
+        
+        fw.writerow(data)
+
+    # update treeview 
+    update_table()
+    # ex_tv.insert('',0, values=data)
+
+    E1.focus()
+        
+    
+
+    # Claer Value
+    v_expense.set('')
+    v_price.set('')
+    v_quantity.set('')
+
+
+############# READ data.CSV FUNCTION #############
+############# READ data.CSV FUNCTION #############
+def read_data_to_label():
+    with open("data.csv", encoding="utf-8") as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        csv_reader = list(csv_reader)
+        text = ''
+        # dt = datetime.now().strftime("%m-%d-%Y")
+        init_date = -6 # Select 6 expense
+        initial_day = csv_reader[init_date][0]
+        text += f"วันที่{csv_reader[init_date][0]}\n"
+        for row in csv_reader[init_date::1]:
+            
+            # if(dt == row[0]):
+            # print(initial_day, row[0])
+            if(initial_day != row[0]):
+                # print("Tranfrom############")
+                text += f"\nวันที่ {row[0]} \n"
+                initial_day = row[0]
+            ##text = '' 
+            #print(f"เวลา {row[0]} รายการ {row[1]} ราคาต่อชิ้น {row[2]} จำนวน {row[3]} ราคารวม {row[4]}")
+            text += f"เวลา: {row[1]} รายการ: {row[2]} ราคาต่อชิ้น: {row[3]} บาท \nจำนวน: {row[4]} ชิ้น ราคารวม: {row[5]} บาท\n"
+
+        # print(text)
+        return text
+
+def read_csv():
+    with open("data.csv", encoding="utf-8") as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        csv_reader = list(csv_reader)
+        # for row in csv_reader[::-1]:
+        #     to_tv.append(row)
+        # return to_tv
+        return csv_reader 
+            
+################ Update Record #####################
+def update_table(): # in treeview
+    ex_tv.delete(*ex_tv.get_children())
+    data = read_csv()
+    # print(data)
+    for e in data:
+        # ex_tv.insert('', 'end', values=e)
+        ex_tv.insert('', 0, values=e)
+
+
+    
+
+
+
+
+
+
+
+#--------------------------------------------------#
+
+################ press Enter to Save ###############
+root.bind("<Return>",Save)
+
+# Built Tab
+tab = ttk.Notebook(root)
+T1 = Frame(tab)
+T2 = Frame(tab)
+T3 = Frame(tab)
+# tab.pack(fill=BOTH, expand=1)
+
+icon_t1 = PhotoImage(file="E:/computer/python/LungEn Beginner/EP4/t1_expense.png")
+icon_t2 = PhotoImage(file="E:/computer/python/LungEn Beginner/EP4/t2_expenselist.png")
+icon_t3 = PhotoImage(file="E:/computer/python/LungEn Beginner/EP4/t3_tv.png")
+icon_b1 = PhotoImage(file="E:/computer/python/LungEn Beginner/EP4/t1_save.png")
+
+i = PhotoImage(file="EP6/money.png")
+
+
+tab.add(T1, text=f"{'Add Expense': ^30}", image=icon_t1, compound='top') # T1 T2 is Frame
+tab.add(T2, text=f"{'Expense List': ^30}", image=icon_t2, compound='top') 
+tab.add(T3, text=f"{'Treeview': ^30}", image=icon_t3, compound='top') 
+
+
+f1 = Frame(T1)
+f1.pack(fill=BOTH)
+
+f2 = Frame(T2)
+f2.pack(fill=BOTH)
+
+f3 = Frame(T3)
+f3.pack(fill=BOTH)
+
+
+# Page 1 ---------------------------------------------------
+Psedu_lb = ttk.Label(f1)
+Psedu_lb.pack()
+
+main_icon = PhotoImage(file="E:/computer/python/LungEn Beginner/EP4/icon_coin.png")
+l_img1 = ttk.Label(f1, image=main_icon)
+l_img1.pack(pady=10)
+
+l1 = ttk.Label(f1, text="รายการค่าใช้จ่าย", font=(None, 20))
+l1.pack(pady=8)
+v_expense = StringVar()
+E1 = ttk.Entry(f1, textvariable=v_expense, font=(None, 16))
+E1.pack(pady=8)
+
+l2 = ttk.Label(f1, text="ราคาต่อชิ้น(บาท)", font=(None, 20))
+l2.pack(pady=8)
+v_price = StringVar()
+E2 = ttk.Entry(f1, textvariable=v_price, font=(None, 16))
+E2.pack(pady=8)
+
+l3 = ttk.Label(f1, text="จำนวน(ชิ้น)", font=(None, 20))
+l3.pack(pady=8)
+v_quantity = StringVar()
+E3 = ttk.Entry(f1, textvariable=v_quantity, font=(None, 16))
+E3.pack(pady=8) # 
+l3_1 = ttk.Label(f1, text=" <Default = 1>", font=(None, 8))
+l3_1.pack()
+
+B1 = ttk.Button(f1, text=f"{'Save': >15}", image=icon_b1, compound="left", command=Save)
+B1.pack(ipadx=30, ipady=20, pady=10)
+#-----------------------------------------------------------
+
+# Page 2 ---------------------------------------------------
+show_today = ttk.Label(f2, text="\nประวัติการใช้จ่าย\n", font=(None,16), foreground="green") # Normal Label fg = "green"
+show_today.pack()
+
+v_show = StringVar()
+v_show.set(read_data_to_label())
+# v_result.set("----------ผลลัพธ์----------")
+show = ttk.Label(f2, textvariable=v_show, font=(None,14)) # Normal Label fg = "green"
+show.pack()
+
+# ----------------------------------------------------------
+
+# Page 3 ---------------------------------------------------
+header = ["date", "time", "expense", "price_piece", "quantity", "total"]
+header_text = ['วันที่', 'เวลา', 'รายการ', 'ราค่าต่อหน่วย', 'จำนวน', 'ราคารวม'] 
+ex_tv = ttk.Treeview(f3, columns=header, show='headings',height=25) # unit of height is number of line
+ex_tv.pack()
+# for hd in header:
+#     	ex_tv.heading(hd,text=hd)
+for hd, hd_t in zip(header, header_text):
+	ex_tv.heading(hd,text=hd_t)
+
+headerwidth = [(70,80),(70,80),(90,100),(70,80),(70,80),(70,80)] # width of header
+
+for hd,W in zip(header,headerwidth):
+	ex_tv.column(hd,minwidth=W[0],width=W[1])
+
+update_table()
+
+# for e in (read_csv()[::-1]): #enumerate 
+#     ex_tv.insert('', END, values=e)
+
+# ----------------------------------------------------------
+
+
+
+v_result = StringVar()
+v_result.set("\n\n----------Developed By Isoon----------")
+result = ttk.Label(f1, textvariable=v_result, font=(None,16), foreground="green") # Normal Label fg = "green"
+result.pack(pady=8)
+
+tab.pack(fill=BOTH, expand=1)
+# n.pack()
+# root.bind("Tab", lambda x: E2.focus())
+
+root.mainloop()
